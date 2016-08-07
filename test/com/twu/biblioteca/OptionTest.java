11792c4784ca995a168c2b819b88929c45e76afe
Gabrielle Saman <gsaman@thoughtworks.com>
2016-08-07 02:18:19 -0300

    Created Option tests and model

---
+++ test/com/twu/biblioteca/OptionTest.java
@@ -0,0 +1,18 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class OptionTest {
+    Option option = new Option(01, "List Books");
+
+    @Test
+    public void getOptionName() {
+        assertEquals("List Books", option.getName());
+    }
+
+    @Test
+    public void getOptionID() {
+        assertEquals(01, option.getId());
+    }
+}
